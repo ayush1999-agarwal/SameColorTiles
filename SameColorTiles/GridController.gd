e23d078ba7305880d53522b6fb1b3a4e7a4ce857
Ayush Agarwal <1999ayush@gmail.com>
2024-08-14 02:26:15 +0530

    Starting Track Generator + Game Win Condition added

--- SameColorTiles/GridController.gd
+++ SameColorTiles/GridController.gd
@@ -6,6 +6,7 @@ export var colorCodes: Array
 onready var gridRow : GridRow = $GridRow
 
 var gridPerBoxState : Array
+var lightColorCount : int
 
 func _ready():
 	gridRow.AddChildren(gridSize)
@@ -13,9 +14,25 @@ func _ready():
 	for column in gridRow.get_children():
 		for button in column.get_children():
 			button.connect("pressed", self, "_on_GridButton_pressed", [button])
-			
+	
+	_generateStartingGrid()
+		
+func _generateStartingGrid():
 	for i in pow(gridSize,2):
-		gridPerBoxState.append(0)
+		var randColorId = randi() % 2
+		if randColorId == 0:
+			lightColorCount += 1
+		
+		#To make sure all boxes do not start with complete state
+		if i == pow(gridSize,2) - 1:
+			if lightColorCount == pow(gridSize,2):
+				lightColorCount -= 1
+				randColorId = 1
+			elif lightColorCount == 0:
+				lightColorCount += 1
+				randColorId = 0
+		gridPerBoxState.append(randColorId)
+		gridRow.get_child(i / gridSize).get_child(i % gridSize).modulate = colorCodes[randColorId]
 
 func _on_GridButton_pressed(which : Button):
 	var pressedButton : Vector2 = Utils.GetGridButtonFromName(which.name)
@@ -25,8 +42,18 @@ func _on_GridButton_pressed(which : Button):
 	
 	for btnId in adjacentBtnIds:
 		_toggleGridBoxColor(btnId)
+	
+	if lightColorCount == 0 || lightColorCount == pow(gridSize,2):
+		print("Game Won")
 
 func _toggleGridBoxColor(btnId : Vector2):
 	#Toggle color by getting reverse of what is stored in per box state
 	gridRow.get_child(btnId[0]).get_child(btnId[1]).modulate = colorCodes[1 - gridPerBoxState[btnId[0] * gridSize + btnId[1]]]
 	gridPerBoxState[btnId[0] * gridSize + btnId[1]] = 1 - gridPerBoxState[btnId[0] * gridSize + btnId[1]]
+
+	#Light Color is represented by 0 element in the array
+	if gridPerBoxState[btnId[0] * gridSize + btnId[1]] == 0:
+		lightColorCount += 1
+	else:
+		lightColorCount -= 1
+	
